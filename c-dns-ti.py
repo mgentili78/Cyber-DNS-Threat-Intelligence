b9419eb0d45a2b6e718f5bac194028a07e7beacd
Mirco Gentili <mirco.gentili@gmail.com>
2017-02-21 22:24:35 +0100

    version 0.2.6

--- c-dns-ti.py
+++ c-dns-ti.py
@@ -13,7 +13,7 @@ import re
 index_name = "packetbeat-*"
 type_name = "dns"
 size_count = 200
-time_delta = "02:15:00"
+time_delta = "00:15:00"
 host = "http://192.168.47.200:9200"
 es = Elasticsearch([host], timeout=100)
 
@@ -23,25 +23,18 @@ time_start = (time_end - timedelta(hours=int(h),minutes=int(m), seconds=int(s)))
 time_start = int(time_start.timestamp()) * 1000
 time_end = int(time_end.timestamp()) * 1000
 
+regex_arpa_ip = r"(\d*)\.(\d*)\.(\d*)\.(\d*)"
 
 dns_query_a = {
     "size": size_count,
-    "_source": "dns",
-    "query": {
+    "_source": ["dns", "ip", "client_ip"],
+     "query": {
         "bool": {
             "must": [
                 {
                     "match": {
                         "dns.question.type": {
                             "query": "A",
-                            "type": "phrase"
-                        }
-                    }
-                },
-                {
-                    "match": {
-                        "status": {
-                            "query": "OK",
                             "type": "phrase"
                         }
                     }
@@ -62,11 +55,9 @@ dns_query_a = {
 
 }
 
-regex_arpa_ip = r"(\d*)\.(\d*)\.(\d*)\.(\d*)"
-
 dns_query_ptr = {
     "size": size_count,
-    "_source": "dns",
+    "_source": ["dns", "ip", "client_ip", "resource"],
     "query": {
         "bool": {
             "must": [
@@ -99,7 +90,8 @@ print(datetime.now().replace(microsecond=0))
 result_query_dns_a = helpers.scan(
     es, index=index_name, doc_type=type_name, query=dns_query_a)
 
-list_ipv4 = []
+dns_record = {}
+list_dns_record = []
 
 for k1 in result_query_dns_a:
     try:
@@ -108,9 +100,16 @@ for k1 in result_query_dns_a:
             try:
                 dns_type = k1["_source"]["dns"]["answers"][k2]["type"]
                 if dns_type == "A":
-                    ipv4 = k1["_source"]["dns"]["answers"][k2]["data"]
-                    if not ipaddress.IPv4Address(ipv4).is_private:
-                        list_ipv4.append(ipv4)
+                    try:
+                        ip_address = k1["_source"]["dns"]["answers"][k2]["data"]
+                        client_ip = k1["_source"]["client_ip"]
+                        name = k1["_source"]["dns"]["answers"][k2]["name"]
+                        dns_server = k1["_source"]["ip"]
+                        dns_record = {'ip_address': ip_address, "name": name, "dns_server": dns_server, "client_ip": client_ip}
+                    except Exception as error:
+                        pass
+                    if not ipaddress.IPv4Address(ip_address).is_private and not dns_record in list_dns_record: 
+                        list_dns_record.append(dns_record)
             except Exception as error:
                 pass
     except Exception as error:
@@ -121,17 +120,25 @@ result_query_dns_ptr = helpers.scan(
 
 for k1 in result_query_dns_ptr:
     try:
-        ipv4_arpa = k1["_source"]["dns"]["question"]["name"]
-        list_oct_ipv4 = list(re.findall(regex_arpa_ip, ipv4_arpa)).pop()[::-1]
-        ipv4 = str.join('.', list_oct_ipv4)
-        if not ipaddress.IPv4Address(ipv4).is_private:
-            list_ipv4.append(ipv4)
+        client_ip = k1["_source"]["client_ip"]
+        dns_server = k1["_source"]["ip"]
+        name = k1["_source"]["resource"]
+        ip_address_arpa = k1["_source"]["dns"]["question"]["name"]
+        list_oct_ip_address = list(re.findall(regex_arpa_ip, ip_address_arpa)).pop()[::-1]
+        ip_address = str.join('.', list_oct_ip_address)
+        dns_record = {'ip_address': ip_address, "name": name, "dns_server": dns_server, "client_ip": client_ip}
+        if not ipaddress.IPv4Address(ip_address).is_private and not dns_record in list_dns_record:
+            list_dns_record.append(dns_record)
     except Exception as error:
         pass
 
-list_ipv4 = list(set(list_ipv4))
+#for i in list_dns_record:
+ #  print(i)
 
-# for i in list_ipv4:
-#   print(i)
-print(len(list_ipv4))
+list_ip = []
+for k1 in list_dns_record:
+    list_ip.append(k1['ip_address'])
+
+list_ip = list(set(list_ip))
+print(len(list_ip))
 print(datetime.now().replace(microsecond=0))
